dc543d017d8a4e09d412f17865da9146cf200c82
MIGUEL FOSSO <115406383+paulino237@users.noreply.github.com>
2026-01-21 09:04:25 -0800

    Update main.ts

--- landing-page/src/index.ts
+++ landing-page/src/index.ts
@@ -1,5 +1,5 @@
 import { renderApp } from 'rasengan/client';
 import App from './main';
-import AppRouter from '@/app/app.router';
+import AppRouter from './app/app.router';
 
 renderApp(App, AppRouter, { reactStrictMode: true });
--- landing-page/src/main.tsx
+++ landing-page/src/main.tsx
@@ -1,5 +1,5 @@
 import '@rasenganjs/image/css';
-import '@/styles/index.css';
+import './styles/index.css';
 import { type AppProps } from 'rasengan';
 
 export default function App({ Component, children }: AppProps) {
